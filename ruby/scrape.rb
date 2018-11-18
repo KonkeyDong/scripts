f20016908d46df6f71dec1c3d82f766eb04c1c5e
John <dittoes.1@gmail.com>
2018-11-18 15:56:40 -0600

    removed shitty method

--- ruby/scrape.rb
+++ ruby/scrape.rb
@@ -14,20 +14,20 @@ class Scrape
     @album = get_album_name
     @number_of_songs = get_number_of_songs.to_i
     @songlist = @html.xpath('//table[@id="songlist"]/tr/td[@class="clickable-row"]')
-    @header_length = get_header_length
     @bad_songs = false
   end
 
   def download
-    return if check_number_of_songs_found
+    songs = pre_download
+    return if check_number_of_songs_found(songs)
 
     pm = Parallel::ForkManager.new(30) # 30 = max procs
 
     FileUtils.mkdir_p @album
-    pre_download.each do |data|
+    songs.each do |data|
       pm.start(data) && next # blocks until new fork slot is available
 
-      puts "Now downloading: [#{data[:title]}]"
+      puts "Now downloading: [#{data[:title]}] (of #{@number_of_songs})"
       file_url = Nokogiri::HTML(open(data[:url])).xpath('//audio').first.attributes["src"].value
       IO.copy_stream(open(file_url), "./#{@album}/#{data[:title]}")
 
@@ -45,26 +45,53 @@ class Scrape
       next if @songlist[i].text =~ /\d+:\d+/ # song length
       next if @songlist[i].text =~ /\d+\.\d+\s[MK]B/i # song size
 
+      song = @songlist[i].text
+              .gsub("'", '')
+              .gsub('-', ' ')
+              .gsub(/\s+/, '_')
+              .gsub(/_$/, '')
+
+      song = "#{song}.mp3" unless song.match(Regexp.new('\.mp3$', Regexp::IGNORECASE))
       data.push(
         {
-          title: @songlist[i].text,
+          title: song,
           url: "https://downloads.khinsider.com#{@songlist[i].children.first.attributes["href"].value}"
         }
       )
     end
 
+    data = add_track_numbers(data) unless has_track_numbers?(data)
+
     return data
   end
 
-  def check_number_of_songs_found
-    return nil if (@songlist.length / @header_length) == @number_of_songs
+  def has_track_numbers?(songs)
+    samples = songs.shuffle.first(10)
+    samples.each do |sample|
+      return true if sample[:title].match(Regexp.new('_?\d{2,4}_'))
+    end
+
+    false
+  end
+
+  def add_track_numbers(songs)
+    new_songs = []
+    for i in 0...@number_of_songs
+      new_songs.push(
+        title: "#{(i + 1).to_s.rjust(3, '0')}_#{songs[i][:title]}",
+        url: songs[i][:url]
+      )
+    end
+    new_songs
+  end
+
+  def check_number_of_songs_found(songs)
+    return nil if songs.length == @number_of_songs
 
     STDERR.puts 'Number of songs mismatch!'
     STDERR.puts "@album: #{@album}"
-    STDERR.puts "@number_of_songs: #{@number_of_songs}"
-    STDERR.puts "found: #{@songlist.length / @header_length}"
+    STDERR.puts "found: #{@songs.length} out of #{@number_of_songs}"
     STDERR.puts "Ignoring..."
-    # error_buffer.push(@url)
     @bad_songs = true
   end
 
@@ -83,10 +110,6 @@ class Scrape
 
   def get_number_of_songs
     extract_data(number_of_songs_regex)
-  end
-
-  def get_header_length
-    @html.xpath('//tr[@id="songlist_header"]').xpath('//th').length / 2
   end
 
   def number_of_songs_regex
