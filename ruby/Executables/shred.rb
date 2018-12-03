b8c6400f517a4cf26dc13584642802c03adf5d7c
John <dittoes.1@gmail.com>
2018-12-02 21:58:27 -0600

    add getopts for --num-threads

--- ruby/Executables/shred.rb
+++ ruby/Executables/shred.rb
@@ -1,20 +1,35 @@
 require 'byebug'
 require 'forkmanager'
+require 'getoptlong'
 require_relative '../Classes/utilities'
 require_relative '../Classes/traverse'
 
-
 # TODO: add cmd options for num_threads
+options = {
+    num_threads: 1
+}
+
+getopts = GetoptLong.new(
+    [ '--num-threads', '-n', GetoptLong::OPTIONAL_ARGUMENT ]
+)
+
+getopts.each do |opt, arg|
+    case opt
+    when '--num-threads'
+        options[:num_threads] = arg.to_i
+        # puts "--num-threads: #{options[:num_threads]}"
+        STDERR.puts "WARNING: over-threading can lower performance, especially on HDDs. Use at your own risk..." if options[:num_threads] > 4
+    end
+end
 
 directory = ARGV
 directory = Dir.getwd if directory.empty?
 
-puts "directory: #{directory}"
 exit 0 unless Utilities::prompt("Do you want to shred everything in the directory [#{directory}]?")
 
 traverse = Traverse.new(directory)
 
-pm = Parallel::ForkManager.new(4) # 4 = max procs
+pm = Parallel::ForkManager.new(options[:num_threads])
 traverse.files.each do |file|
     pm.start(file) && next
 
