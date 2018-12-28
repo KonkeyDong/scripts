94448561a0bdb42af67747335ac5aabd442c71aa
John <dittoes.1@gmail.com>
2018-12-27 23:35:11 -0600

    add help doc for command line args

--- ruby/Executables/shred.rb
+++ ruby/Executables/shred.rb
@@ -4,22 +4,36 @@ require 'getoptlong'
 require_relative '../Classes/utilities'
 require_relative '../Classes/traverse'
 
-# TODO: add cmd options for num_threads
 options = {
     num_threads: 4
 }
 
 getopts = GetoptLong.new(
-    [ '--num-threads', '-n', GetoptLong::OPTIONAL_ARGUMENT ]
+    [ '--num-threads', GetoptLong::OPTIONAL_ARGUMENT ],
+    [ '--help', '-h', GetoptLong::OPTIONAL_ARGUMENT ]
 )
 
 getopts.each do |opt, arg|
     case opt
     when '--num-threads'
         options[:num_threads] = arg.to_i
-        # puts "--num-threads: #{options[:num_threads]}"
+        puts "--num-threads set to [#{options[:num_threads]}]"
         STDERR.puts "WARNING: over-threading can lower performance, especially on HDDs. Use at your own risk..." if options[:num_threads] > 4
+    when '--help', '-h'
+        puts <<~HEREDOC
+        --- COMMAND LINE ARGS ---
+            --num-threads n
+                Sets the numbers of threads n for parallel execution.
+                Defaults to 4.
+                Warning: avoid using many threads on HDDs.
+    
+            --help
+                Display this help doc and exit.
+        HEREDOC
+        exit 0
     end
+
+
 end
 
 directory = ARGV
@@ -34,6 +48,9 @@ traverse.files.each do |file|
     pm.start(file) && next
 
     puts "Shredding file: [#{file}]"
+
+    # TODO: maybe make a simple shred utility in ruby? Not sure;
+    # the C code looks sophisticated, but it would add portability.
     %x(/usr/bin/shred -uz "#{file}")
 
     pm.finish(0)
@@ -43,4 +60,4 @@ pm.wait_all_children
 puts "Removing directories..."
 traverse.directories.each { |directory| Dir.rmdir(directory) }
 
-exit 0 # success
+exit 0
