f362a2211f01e8b480ec6deaae26a05d7bb03770
Jermafenser <dittoes.1@gmail.com>
2019-09-02 18:35:09 -0500

    Update run_me.sh

--- setup/run_me.sh
+++ setup/run_me.sh
@@ -43,6 +43,8 @@ echo "Copying config directory ..."
 mkdir -p ~/.config
 cp -r ${GIT_CONFIG_DIR}/* ${CONFIG_DIR}
 
+echo "run [download_vscode.sh] to download VS Code separetly.
+
 # add aliases
 source ${BASH_RC}
 
