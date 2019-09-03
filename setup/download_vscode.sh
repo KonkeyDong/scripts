5441433aea6aad7ed48b3abcd38b1f1e60141e0f
Jermafenser <dittoes.1@gmail.com>
2019-09-02 19:05:58 -0500

    Update download_vscode.sh

--- setup/download_vscode.sh
+++ setup/download_vscode.sh
@@ -5,3 +5,6 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update ; sudo apt install code
+
+# download extensions
+code --install-extension avli.clojure DavidAnson.vscode-markdownlint rebornix.ruby shd101wyy.markdown-preview-enhanced
