25f3a3b92d2d4b04fb811da957e4c9d0542e24e9
konkeydong <dittoes.1@gmail.com>
2020-11-18 16:21:53 -0600

    improve stopwatch

--- ruby/backup_youtube_videos/stopwatch.rb
+++ ruby/backup_youtube_videos/stopwatch.rb
@@ -11,9 +11,35 @@ class Stopwatch
     elapsed = now - @start
     puts 'Started: ' + @start.to_s
     puts 'Now: ' + now.to_s
-    puts 'Elapsed time: ' +  elapsed.to_s + ' seconds'
-    elapsed.to_s
-  end
+    #puts 'Elapsed time: ' +  elapsed.to_s + ' seconds'
+    result = {
+        decimal: 0,
+        seconds: 0,
+        minutes: 0,
+        hours:   0,
+        days:    0,
+    }
 
+    result[:decimal] = ((elapsed - elapsed.truncate) * 1000).truncate
+    elapsed = elapsed.truncate
+
+
+    if (elapsed > 60)
+        result[:seconds] = elapsed % 60
+        elapsed = elapsed / 60
+    end
+
+    if (elapsed > 60)
+        result[:minutes] = elapsed % 60
+        elapsed = elapsed / 60
+    end
+
+    if (elapsed > 24)
+        result[:hours] = elapsed % 24
+        result[:days] = elapsed / 24
+    end
+
+    puts "Elapsed time: #{result[:days]} days, #{result[:hours]}H #{result[:minutes]}M #{result[:seconds]}.#{result[:decimal]}S"
+  end
 end
 
