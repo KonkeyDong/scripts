dfe40f706a24b38e7dc39534e743dbf5ebe9af4b
konkeydong <dittoes.1@gmail.com>
2021-04-01 16:26:18 -0500

    add archive

--- ruby/backup_manga/download.rb
+++ ruby/backup_manga/download.rb
@@ -14,7 +14,7 @@ def exit_error
 end
 
 def pre_download(url)
-    html = Nokogiri::HTML(URI.open(url))
+    html = Nokogiri::HTML(open(url))
     html.css('.chapter-name.short')
                    .reverse
                    .map do |chapter|
@@ -30,23 +30,36 @@ def download(chapters_and_href, book_name)
     chapters_and_href.each do |data|
         title = data[:title]
         href = data[:href]
-        
+
+        puts "Downloading #{title}..."
+
         directory = [BASE_DIRECTORY_PATH, book_name, title].join("/")
-        # puts directory
+        archive = [BASE_DIRECTORY_PATH, book_name, "archive.txt"].join("/")
+
         FileUtils.mkdir_p(directory)
 
-        html = Nokogiri::HTML(URI.open(href)).css('.sl-page option')        
+        # look through archive to avoid unnecessary rewrites.
+        FileUtils.touch(archive) # create file if it doesn't exist
+        archive_hash = File.open(archive).readlines.map(&:chomp).map { |url| [url, true]}.to_h
+
+        html = Nokogiri::HTML(open(href)).css('.sl-page option')
         html[0...(html.length / 2)].each_with_index do |page, index|
-            image_url = Nokogiri::HTML(URI.open(page["value"])).css('.manga_pic').first["src"]
+            image_url = Nokogiri::HTML(open(page["value"])).css('.manga_pic').first["src"]
             file_extension = image_url.match(/\.(\w+)$/).captures.first.downcase
             file_name = (index + 1).to_s.rjust(3, "0") + ".#{file_extension}"
-            
+
             # write image file
             # puts "#{directory}/#{file_name} | #{image_url}"
-            open(image_url) do |image|
-                File.open("#{directory}/#{file_name}", "wb") do |file|
-                    file.write(image.read)
+            if !archive_hash[image_url]
+                open(image_url) do |image|
+                    File.open("#{directory}/#{file_name}", "wb") do |file|
+                        file.write(image.read)
+                    end
                 end
+
+                File.write(archive, "#{image_url}\n", mode: 'a')
+            else
+                puts "Skipping [#{file_name}] as it is in the archive!"
             end
         end
     end
