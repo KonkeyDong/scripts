49aa8559998887d4a1a99e7dcc0a9c43c97a641f
konkeydong <dittoes.1@gmail.com>
2021-04-01 22:11:27 -0500

    fix script

--- ruby/backup_manga/download.rb
+++ ruby/backup_manga/download.rb
@@ -7,14 +7,10 @@ require 'nokogiri'
 require_relative './url_data'
 
 BASE_DIRECTORY_PATH = '/media/HDD_4TB_01/manga'
-
-def exit_error
-    STDERR.puts "Missing a file path! Aborting..."
-    exit 1
-end
+READ_TIEMOUT = 30
 
 def pre_download(url)
-    html = Nokogiri::HTML(open(url))
+    html = Nokogiri::HTML(open(url, read_timeout: READ_TIEMOUT))
     html.css('.chapter-name.short')
                    .reverse
                    .map do |chapter|
@@ -26,41 +22,46 @@ def pre_download(url)
                    end
 end
 
-def download(chapters_and_href, book_name)
+def pad_chapter_numbers(title)
+    chapter, number = title.split('_')
+    pad_by_this_amount = number =~ /\./ ? 6 : 4
+
+    [chapter, number.rjust(pad_by_this_amount, "0")].join('_')
+end
+
+def download(chapters_and_href, book_name, archive, archive_hash)
     chapters_and_href.each do |data|
-        title = data[:title]
+        title = pad_chapter_numbers(data[:title])
         href = data[:href]
 
         puts "Downloading #{title}..."
-
         directory = [BASE_DIRECTORY_PATH, book_name, title].join("/")
-        archive = [BASE_DIRECTORY_PATH, book_name, "archive.txt"].join("/")
-
         FileUtils.mkdir_p(directory)
 
-        # look through archive to avoid unnecessary rewrites.
-        FileUtils.touch(archive) # create file if it doesn't exist
-        archive_hash = File.open(archive).readlines.map(&:chomp).map { |url| [url, true]}.to_h
-
-        html = Nokogiri::HTML(open(href)).css('.sl-page option')
+        html = Nokogiri::HTML(open(href, read_timeout: READ_TIEMOUT)).css('.sl-page option')
         html[0...(html.length / 2)].each_with_index do |page, index|
-            image_url = Nokogiri::HTML(open(page["value"])).css('.manga_pic').first["src"]
+            page_url = page["value"]
+
+            if archive_hash[page_url]
+                name = (index + 1).to_s.rjust(4, "0") + ".image"
+                puts "Skipping [#{name}] as it has already been downloaded."
+                next
+            end
+
+            image_url = Nokogiri::HTML(open(page_url, read_timeout: READ_TIEMOUT)).css('.manga_pic').first["src"]
             file_extension = image_url.match(/\.(\w+)$/).captures.first.downcase
             file_name = (index + 1).to_s.rjust(3, "0") + ".#{file_extension}"
 
             # write image file
             # puts "#{directory}/#{file_name} | #{image_url}"
-            if !archive_hash[image_url]
-                open(image_url) do |image|
-                    File.open("#{directory}/#{file_name}", "wb") do |file|
-                        file.write(image.read)
-                    end
+            open(image_url) do |image|
+                File.open("#{directory}/#{file_name}", "wb") do |file|
+                    file.write(image.read)
                 end
-
-                File.write(archive, "#{image_url}\n", mode: 'a')
-            else
-                puts "Skipping [#{file_name}] as it is in the archive!"
             end
+
+            # update the archive file
+            File.write(archive, "#{page_url}\n", mode: 'a')
         end
     end
 end
@@ -68,7 +69,13 @@ end
 URL_DATA.each do |(url, book_name)|
     puts "Now downloading [#{book_name}]..."
     chapters_and_href = pre_download(url)
-    download(chapters_and_href, book_name)
+
+    # look through archive to avoid unnecessary rewrites.
+    archive = [BASE_DIRECTORY_PATH, book_name, "archive.txt"].join("/")
+    FileUtils.touch(archive) # create file if it doesn't exist
+    archive_hash = File.open(archive).readlines.map(&:chomp).map { |url| [url, true]}.to_h
+
+    download(chapters_and_href, book_name, archive,  archive_hash)
 end
 
 puts "Complete!"
