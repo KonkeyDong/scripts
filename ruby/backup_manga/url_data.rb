640e019407560772dff9d9c1076930ffac4e297e
konkeydong <dittoes.1@gmail.com>
2021-04-14 17:13:09 -0500

    remove

--- ruby/backup_manga/url_data.rb
+++
@@ -1,9 +0,0 @@
-# An array of arrays.
-# The first argument of the inner array is the URL.
-# The second argument is the name of the book.
-
-URL_DATA = [
-    #["http://www.tenmanga.com/book/Dragon+Ball.html", "dragon_ball"],
-    ["http://www.tenmanga.com/book/Attack+On+Titan+-+Before+The+Fall.html", "attack_on_titan_-_before_the_fall"],
-    ["http://www.tenmanga.com/book/Yu+Yu+Hakusho.html", "yu_yu_hakusho"],
-]
--- ruby/backup_youtube_videos/config.rb
+++
@@ -1,6 +0,0 @@
-module Config
-    YOUTUBE_DL_BASE = "/usr/local/bin/youtube-dl --ignore-errors"
-    HDD_DESTINATION_BASE = "/media/HDD_4TB_01/YouTube"
-    OLD_FILE_FORMAT = "%(title)s.%(ext)s" 
-    DESIRED_FILE_FORMAT = "%(upload_date)s_%(title)s.%(ext)s"
-end
